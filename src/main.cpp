de274dce9beb67c69dcce5fbe6380b12c4e6aa2d
MrSteelRat <drebin341@gmail.com>
2024-01-28 17:58:01 +0300

    add src/CMakeLitst & main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <tgbot/tgbot.h>
+
+#include "BaseCommandBot.h"
+
+int main()
+{
+  // Class start bot
+  BaseCommandBot myBot;
+  myBot.baseCommands();
+
+  // Class parsing module
+
+  // Class DB module
+  //const std::string dbFileName = "bot.db";
+  // Database database(dbFileName);
+  return 0;
+}
