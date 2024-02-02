1287036b59fb108a7a4d7198f81132e64015da51
MrSteelRat <drebin341@gmail.com>
2024-02-02 23:00:17 +0300

    Ref Class Base CommandBot

--- module/BaseCommandBot.cpp
+++ module/BaseCommandBot.cpp
@@ -1,23 +1,38 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java:
+// http://www.viva64.com
 //
 // Created by alex on 11/13/23.
 //
+
 #include <tgbot/tgbot.h>
+
+#include "token.h"
+#include "links_user_data.h"
 #include "BaseCommandBot.h"
 #include "DataBase.h"
-#include "links_user_data.h"
-#include "token.h"
 
 const std::string dbFileName = "body_therapy.db";
 
-BaseCommandBot::BaseCommandBot() : m_bot(TOKEN), m_database(dbFileName), m_databaseCreaated(false) {}
+BaseCommandBot::BaseCommandBot()
+    : m_bot(TOKEN), m_database(dbFileName), m_databaseCreaated(false)
+{
+}
 
-void BaseCommandBot::createDatabase(int userId, const std::string& username) {
-    m_database.createUser(userId, username);
-    m_databaseCreaated = true;
+void BaseCommandBot::createDatabase(int userId, const std::string& username)
+{
+  m_database.createUser(userId, username);
+  m_databaseCreaated = true;
 }
 
 void BaseCommandBot::baseCommands()
 {
+  auto sendMessage = [this](const std::string& command,
+                            const std::string& url,
+                            TgBot::Message::Ptr message)
+  { m_bot.getApi().sendMessage(message->chat->id, url); };
+
   m_bot.getEvents().onCommand("start",
                               [this](TgBot::Message::Ptr message)
                               {
@@ -28,40 +43,36 @@ void BaseCommandBot::baseCommands()
                                                            HELLO_MESSAGE);
                               });
 
-  m_bot.getEvents().onCommand(
-      "vk",
-      [this](TgBot::Message::Ptr message)
-      { m_bot.getApi().sendMessage(message->chat->id, URL_VK); });
+  m_bot.getEvents().onCommand("vk",
+                              [sendMessage](TgBot::Message::Ptr message)
+                              { sendMessage("vk", URL_VK, message); });
+
+  m_bot.getEvents().onCommand("tg",
+                              [sendMessage](TgBot::Message::Ptr message)
+                              { sendMessage("tg", URL_TG, message); });
+
+  m_bot.getEvents().onCommand("github",
+                              [sendMessage](TgBot::Message::Ptr message)
+                              { sendMessage("github", UTL_GITHUB, message); });
+
+  m_bot.getEvents().onCommand("youtube",
+                              [sendMessage](TgBot::Message::Ptr message) {
+                                sendMessage("youtube", URL_YOUTUBE, message);
+                              });
+
+  m_bot.getEvents().onCommand("boosty",
+                              [sendMessage](TgBot::Message::Ptr message)
+                              { sendMessage("boosty", URL_BOOSTY, message); });
 
   m_bot.getEvents().onCommand(
-      "tg",
-      [this](TgBot::Message::Ptr message)
-      { m_bot.getApi().sendMessage(message->chat->id, URL_TG); });
-
-  m_bot.getEvents().onCommand(
-      "github",
-      [this](TgBot::Message::Ptr message)
-      { m_bot.getApi().sendMessage(message->chat->id, UTL_GITHUB); });
-
-  m_bot.getEvents().onCommand(
-      "youtube",
-      [this](TgBot::Message::Ptr message)
-      { m_bot.getApi().sendMessage(message->chat->id, URL_YOUTUBE); });
-
-  m_bot.getEvents().onCommand(
-      "boosty",
-      [this](TgBot::Message::Ptr message)
-      { m_bot.getApi().sendMessage(message->chat->id, URL_BOOSTY); });
-
-    m_bot.getEvents().onCommand(
       "donat_alert",
-      [this](TgBot::Message::Ptr message)
-      { m_bot.getApi().sendMessage(message->chat->id, URL_DONAT_ALERT); });   
+      [sendMessage](TgBot::Message::Ptr message)
+      { sendMessage("donat_alert", URL_DONAT_ALERT, message); });
 
-    m_bot.getEvents().onCommand(
-      "website",
-      [this](TgBot::Message::Ptr message)
-      { m_bot.getApi().sendMessage(message->chat->id, URL_WEBSITE); });    
+  m_bot.getEvents().onCommand("website",
+                              [sendMessage](TgBot::Message::Ptr message) {
+                                sendMessage("website", URL_WEBSITE, message);
+                              });
 
   m_bot.getEvents().onAnyMessage(
       [this](TgBot::Message::Ptr message)
@@ -72,7 +83,6 @@ void BaseCommandBot::baseCommands()
           return;
         }
       });
-
   try
   {
     std::cout << "Bot username: " << m_bot.getApi().getMe()->username
