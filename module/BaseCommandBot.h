f4fc8e20b5900bbc6a8c796a0d3e7ab037ec8db0
MrSteelRat <drebin341@gmail.com>
2024-02-19 16:10:08 +0300

    swap tgbot lib position Remove PVS comment

--- module/BaseCommandBot.h
+++ module/BaseCommandBot.h
@@ -1,3 +1,8 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java:
+// http://www.viva64.com
+
 //
 // Created by alex on 11/13/23.
 //
@@ -5,10 +10,10 @@
 #ifndef SOFI_SI_BODY_THERAPY_BASECOMMANDBOT_H
 #define SOFI_SI_BODY_THERAPY_BASECOMMANDBOT_H
 
+#include <tgbot/tgbot.h>
+
 #include <iostream>
 #include <string>
-
-#include <tgbot/tgbot.h>
 
 #include "DataBase.h"
 
@@ -20,9 +25,9 @@ class BaseCommandBot
 
  private:
   TgBot::Bot m_bot;
-  Database m_database; 
+  Database m_database;
   bool m_databaseCreaated;
-  
+
   void createDatabase(int userId, const std::string& username);
 };
 
