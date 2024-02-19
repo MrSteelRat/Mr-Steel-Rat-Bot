dda45135d2213f204a2a0d8f8b763c9aea8aefd9
MrSteelRat <drebin341@gmail.com>
2024-02-19 16:08:10 +0300

    clear main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,7 @@
 
 int main()
 {
-  // Class start bot
   BaseCommandBot myBot;
   myBot.baseCommands();
-
-  // Class parsing module
-
-  // Class DB module
-  //const std::string dbFileName = "bot.db";
-  // Database database(dbFileName);
   return 0;
 }
